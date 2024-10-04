e70dc0bcf60717af33c4f9a5220a3a2abe8c7c06
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-10-04 23:08:47 +0500

    Here middleware added

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,29 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from 'express';
 export const app =express();
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+dotenv.config();
+app.use(express.json({limit:"50mb"}));
+
+app.use(cookieParser());
+
+app.use(cors({origin: process.env.ORIGIN, credentials: true}));
+
+app.get("/", (req:Request, res:Response, next:NextFunction) => {
+    res.status(200)
+    .json({
+        success:true,
+        message:"Welcome to the API",
+    })
+});     
+
+
+
+app.all("*", (req:Request, res:Response, next:NextFunction) => {
+    const err= new Error("Page Not Found");
+    // err.statusCode = 404;
+    next(err);
+
+});    
