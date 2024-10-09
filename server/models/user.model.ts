0964df0e15f99ea38db82611bedcd3e702054e69
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-10-09 15:29:48 +0500

    User Model updated final-1

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -7,7 +7,7 @@ export interface IUser extends Document{
      name:string;
      email:string;
      password: string;
-     avater: {
+     avatar: {
             public_id:string;
             url:string;
      },
@@ -18,67 +18,68 @@ export interface IUser extends Document{
      
 }
 
-
 const userSchema = new Schema<IUser>({
-    name:{
-        type:String,
-        required:[true,"Please enter your name"],
-        maxLength:[30,"Your name cannot exceed 30 characters"]
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Your name cannot exceed 30 characters"]
     },
-    email:{
-        type:String,
-        required:[true,"Please enter your email"],
-        unique:true,
-        match:[EmailRegexPattern,"Please enter a valid email address"]
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        match: [EmailRegexPattern, "Please enter a valid email address"]
     },
-    password:{
-        type:String,
-        required:[true,"Please enter your password"],
-        minlength:[6,"Your password must be longer than 6 characters"],
-        select:false
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minlength: [6, "Your password must be longer than 6 characters"],
+        select: false
     },
-    avater:{
-        public_id:{
-            type:String,
-            required:true
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
         },
-        url:{
-            type:String,
-            required:true
+        url: {
+            type: String,
+            required: true
         }
     },
-    role:{
-        type:String,
-        default:"user"
+    role: {
+        type: String,
+        default: "user"
     },
-    isVerified:{
-        type:Boolean,
-        default:false
+    isVerified: {
+        type: Boolean,
+        default: false
     },
-    courses:[
+    courses: [
         {
-            courseId:{
-                type:mongoose.Schema.Types.ObjectId,
-                ref:"Course"
+            courseId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Course"
             }
         }
     ]
-},{timestamps:true});
+}, { timestamps: true });
 
 
 //Hash Password 
-userSchema.pre<IUser>("save",async function(next){
-    if(!this.isModified("password")){
+userSchema.pre<IUser>("save", async function(next) {
+    if (!this.isModified("password")) {
         next();
     }
-    this.password = await bcrypt.hash(this.password,10);
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 
+
 //Compare user password
-userSchema.methods.comparePassword = async function(enteredPassword:string){
-    return await bcrypt.compare(enteredPassword,this.password);
-}
+userSchema.methods.comparePassword = async function(enteredPassword: string) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 
 
 //export 
