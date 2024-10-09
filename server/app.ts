d8476c68fa3f63cf5020632dbd85a26c0e1e0f36
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-10-09 14:26:18 +0500

    User Model created

--- server/app.ts
+++ server/app.ts
@@ -8,7 +8,7 @@ import {errorMiddleware} from './middleware/errors';
 dotenv.config();
 
 export const app = express();
-
+ 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
