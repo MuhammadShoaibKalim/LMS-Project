2c2ea5630bb08c3a549620083a01f98b97b24c08
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-10-12 18:14:41 +0500

    Changes in .env  file, user controller

--- server/app.ts
+++ server/app.ts
@@ -6,7 +6,7 @@ import {errorMiddleware} from './middleware/errors';
 
 
 dotenv.config();
-
+ 
 export const app = express();
  
 app.use(express.json({ limit: "50mb" }));
@@ -26,7 +26,7 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
     res.status(404);
     next(err);
 
-});
+}); 
 
 
 app.use(errorMiddleware);
--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -1,58 +1,48 @@
-import express, {Request, Response, NextFunction} from "express";
-import userModel, {IUser} from "../models/user.model";
+import express, { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
 import { catchAsyncError } from "../middleware/catchAsyncError";
-import ErrorHandler  from "../utils/ErrorHandler";
+import ErrorHandler from "../utils/ErrorHandler";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
-
 
 interface IRegistrationBody {
     name: string;
     email: string;
     password: string;
-    avatar?:string;
+    avatar?: string;
 }
 
-export const registerUser = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
-      try {
-          const {name, email, password} = req.body;
- 
-    const isEmailExist  = await userModel.findOne({email});
-      if(isEmailExist){
-        return next(new ErrorHandler("Email is already exist", 400));
-      }
+export const registerationUser = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
+    const isEmailExist = await userModel.findOne({ email });
 
-      const user : IRegistrationBody = {
-         name,
-         email,
-         password
-      };
+    if (isEmailExist) {
+        return next(new ErrorHandler("Email already exists", 400));
+    }
 
-      const activationToken = createActivationToken(user);
+    const user: IRegistrationBody = { name, email, password }; 
+    const { token, activationCode } = createActivationToken(user);
 
+    res.status(201).json({
+        success: true,
+        message: "User registered successfully, activation code sent",
+        activationCode,
+        token,
+    });
+});
 
-      } catch (error:any) {
-        return next(new ErrorHandler(error.message, 500));
-      }
+interface IActivationToken {
+    token: string;
+    activationCode: string;
+}
+
+export const createActivationToken = (user: IRegistrationBody): IActivationToken => { 
+    const activationCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const token = jwt.sign({ user, activationCode }, process.env.ACTIVATION_SECRET, {
+        expiresIn: "10m",
     });
 
-    interface IActivationToken {
-         token :string,
-         activationCode: string;
-    }
-
-    export const createActivationToken  = (user:IUser) : IActivationToken =>{
-
-      const activationCode = Math.floor(100000 + Math.random() * 900000).toString();
-
-      const token = jwt.sign({user, activationCode}, 
-        process.env.ACTIVATION_SECRETE, {
-        expiresIn: "10m"
-      });
-
-
-      return { token, activationCode};
-    }
-
-    
+    return { token, activationCode };
+};
