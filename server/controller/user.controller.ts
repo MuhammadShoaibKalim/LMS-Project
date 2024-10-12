281176b476bbed4139688bb252c54f736032711b
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-10-12 21:04:58 +0500

    controller major job adding

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -1,52 +1,63 @@
-import express, { Request, Response, NextFunction } from "express";
-import userModel, { IUser } from "../models/user.model";
+import express, {Request, Response, NextFunction} from "express";
+import userModel, {IUser} from "../models/user.model";
 import { catchAsyncError } from "../middleware/catchAsyncError";
-import ErrorHandler from "../utils/ErrorHandler";
+import ErrorHandler  from "../utils/ErrorHandler";
 import jwt from "jsonwebtoken";
+import ejs from "ejs";
 import dotenv from "dotenv";
 dotenv.config();
+
 
 interface IRegistrationBody {
     name: string;
     email: string;
     password: string;
-    avatar?: string;
+    avatar?:string;
 }
 
-export const registerationUser = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+export const registerationUser = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {  
+      try {
+          const {name, email, password} = req.body;
+          const isEmailExist  = await userModel.findOne({email});
 
-    const { name, email, password } = req.body;
-    const isEmailExist = await userModel.findOne({ email });
+      if(isEmailExist){
+        return next(new ErrorHandler("Email is already exist", 400));
+      }
 
-    if (isEmailExist) {
-        return next(new ErrorHandler("Email already exists", 400));
+      const user : IRegistrationBody = {
+         name,
+         email,
+         password,
+      };
+
+      const activationToken = createActivationToken(user);
+      const activationCode = activationToken.activationCode;
+
+      const data = { user: { name: user.name}, activationCode };
+
+
+      } catch (error:any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    });
+
+    interface IActivationToken {
+         token :string,
+         activationCode: string;
     }
 
-    const user: IRegistrationBody = { name, email, password }; 
-    const { token, activationCode } = createActivationToken(user);
+    export const createActivationToken  = (user:any) : IActivationToken =>{
 
-    res.status(201).json({
-        success: true,
-        message: "User registered successfully, activation code sent",
-        activationCode,
-        token,
-    });
-});
+      const activationCode = Math.floor(100000 + Math.random() * 900000).toString();  
 
-interface IActivationToken {
-    token: string;
-    activationCode: string;
-}
-
-export const createActivationToken = (user: IRegistrationBody): IActivationToken => { 
-    const activationCode = Math.floor(100000 + Math.random() * 900000).toString();
-
-    const token = jwt.sign(
-      { user: { name: user.name, email: user.email }, activationCode }, 
-      process.env.ACTIVATION_SECRET as string, 
-      { expiresIn: "10m" }
+      const token = jwt.sign(
+        { user: { name: user.name, email: user.email }, activationCode }, 
+        process.env.ACTIVATION_SECRET as string, 
+        { expiresIn: "10m" }
     );
-    
 
-    return { token, activationCode };
-};
+
+      return { token, activationCode};
+    }
+
+    
